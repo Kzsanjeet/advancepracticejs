74fd3b8ce45052b50b3233acece82bfa5fa15bf6
Kzsanjeet <127291186+Kzsanjeet@users.noreply.github.com>
2023-07-17 22:34:03 +0545

    added for loops and object

---
+++ for.js
@@ -0,0 +1,40 @@
+// //Problem number one
+// let marks = {
+//     harry: 90,
+//     shubham: 9,
+//     lovis: 56,
+//     monika:4
+// }
+
+// for ( let i = 0; i < Object.keys(marks).length; i++){
+//     console.log(Object.keys(marks)[i]+"are"+ Object.values(marks)[i])
+// }
+
+// let list = {
+//     harry: 90,
+//     shubham: 9,
+//     lovis: 56,
+//     monika:4
+
+// }
+// for ( let  i = 0; i < Object.keys(list).length; i++ ){
+//     console.log(Object.keys(list)[i]+" and "+Object.values(list)[i])
+// }
+// //for in loops
+// for (let key in list){
+//     console.log(marks[key])
+// }
+
+// cn = 4
+// let u = prompt("Enter the number:")
+// let i 
+// while (i != cn){
+//     console.log("Okay dude")
+// }
+
+let mean = (a,b,c,d)=>{
+    let mean = r+c+v+e / 5 
+    return mean
+
+}
+ console.log(mean(1,2,3,4))
